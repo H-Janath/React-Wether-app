7f7cf62446259c6915e50436da4d451303c9774d
janath <janathhma@gmail.com>
2024-02-25 17:41:16 +0530

    start project from initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
 import './App.css'
-import Search from './component/search/search'
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const handleOnSearchChange = (SearchData) => {
-    console.log(SearchData)
-  }
+ 
   return (
-    <>
-    <Search onSearchChange={handleOnSearchChange}/>
-       
-    </>
+    <></>
   )
 }
 

b4c465abeb9d208fe9c96ae8240f372318e85bc6
janath <janathhma@gmail.com>
2024-02-25 10:50:06 +0530

    add handleFetchWeather() arrow function

--- src/component/main-card.jsx
+++ src/component/main-card.jsx
@@ -8,6 +8,14 @@ function WeatherForecast() {
         setCity(event.target.value);
     };
 
+    const handleFetchWeather = () => {
+        fetchWeatherData(city);
+    }
+
+   
+
+   
+
     return (
         <div className="container py-5 h-100">
             <div className="row d-flex justify-content-center align-items-center h-100">

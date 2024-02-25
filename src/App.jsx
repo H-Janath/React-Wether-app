1899c4f6a40beb915598a828c447c5aadce3ef99
janath <janathhma@gmail.com>
2024-02-25 17:45:33 +0530

    add boootstrap wether card html structre

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import MainCard from './assets/componenets/mainCard'
 
 
 
@@ -6,7 +7,9 @@ import './App.css'
 function App() {
  
   return (
-    <></>
+    <>
+    <MainCard/>
+    </>
   )
 }
 
